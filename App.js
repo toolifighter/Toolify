fe3f5c2abc7720e4264db0a6725efe5a9f49a620
ericjkl <44269049+ericjkl@users.noreply.github.com>
2018-11-03 23:16:41 +0100

    new navigation; drawer added

--- App.js
+++ App.js
@@ -1,55 +1,57 @@
 // Zinsrechner dazu gepsckt
 
 import React from 'react';
-import {createBottomTabNavigator} from 'react-navigation';
-import BMI from './sources/bmi-screen'
-import Calories from './sources/calories-screen';
-import Measure from './sources/measure';
-import Measure2 from './sources/measure2';
-import Zinsber from './sources/zins';
+import {View, SafeAreaView, ScrollView, Dimensions, Image} from 'react-native';
+import {createDrawerNavigator, DrawerItems} from 'react-navigation';
+import BodyNav from './sources/screens/screens-body/body-index';
+import MeasureNav from './sources/screens/screens-measure/measure-index';
+import MoneyNav from './sources/screens/screens-money/money-index';
 import { MaterialIcons, Entypo  } from '@expo/vector-icons';
 
-const AppNav = createBottomTabNavigator ({
-  Kalorien: {
-    screen: Calories,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <MaterialIcons name="fitness-center" size={26} color={tintColor} />
-      },
-    },
-  },
-  BMI: {
-    screen: BMI,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <MaterialIcons name="accessibility" size={26} color={tintColor} />
-      },
-    },
-  },
-  Messen: {
-    screen: Measure,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <Entypo name="ruler" size={26} color={tintColor} />
-      },
-    },
-  },
-  'Messen 2': {
-    screen: Measure2,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <Entypo name="ruler" size={26} color={tintColor} />
-      },
-    },
-  },
-  Zinsen: {
-    screen: Zinsber,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <Entypo name="ruler" size={26} color={tintColor} />
-      },
-    },
-  },
-})
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppDrawerNavigator/>      
+    )
+  }
+}
 
-export default AppNav;
+const CustomAppDrawer = (props) => (
+  <SafeAreaView style={{flex: 1}}>
+    <View style={{height: 150, alignItems: 'center', justifyContent: 'center', marginTop: 40}} >
+      <Image source={require('./assets/icon.png')} style={{height: 120, width: 120}} />
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+)
+
+const AppDrawerNavigator = createDrawerNavigator({
+  'Körper ': {
+    screen: BodyNav,
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => {
+        return <MaterialIcons name="accessibility" size={22} color={tintColor} />
+      }
+    }
+  },
+  'Messen ' : {
+    screen: MeasureNav,
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => {
+        return <Entypo name="ruler" size={22} color={tintColor} />
+      }
+    }
+  },
+  'Geld ': {
+    screen: MoneyNav,
+    navigationOptions : {
+      drawerIcon: ({ tintColor }) => {
+        return <MaterialIcons name="attach-money" size={22} color={tintColor} />
+      },
+    }
+  },
+},{
+  contentComponent: CustomAppDrawer
+})
